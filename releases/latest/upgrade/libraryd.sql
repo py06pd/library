c85999b41c35aac04bad59242cfe5743ccd5399d
Peter Dawson <py06pd@hotmail.co.uk>
2018-10-17 13:16:13 +0100

    Add new table to upgrade script

--- releases/latest/upgrade/libraryd.sql
+++ releases/latest/upgrade/libraryd.sql
@@ -4,3 +4,17 @@ ALTER TABLE userbook ALTER COLUMN giftfromid drop not null;
 
 UPDATE users SET "role" = 'ROLE_ADMIN' WHERE "role" = 'admin';
 UPDATE users SET "role" = 'ROLE_USER' WHERE "role" = 'anon';
+
+CREATE TABLE groupuser
+(
+    id integer NOT NULL,
+    userid integer NOT NULL,
+    CONSTRAINT groupuser_pkey PRIMARY KEY (id, userid)
+);
+
+CREATE TABLE user_group
+(
+    group_id series NOT NULL,
+    group_name character varying(64) NOT NULL,
+    CONSTRAINT user_group_pkey PRIMARY KEY (group_id)
+);
